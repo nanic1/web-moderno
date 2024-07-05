290cc4772f37edd301777648ca161f76d1132260
nanic <131386211+nanic1@users.noreply.github.com>
2024-07-05 10:51:58 -0300

    Add files via upload

---
+++ modulo 1/dados3.js
@@ -0,0 +1,9 @@
+let idade = 31;
+let salario = 4578.32;
+let estaChovendo = true // ou false
+
+console.log(typeof 31);
+console.log(typeof idade);
+console.log(typeof salario);
+console.log(typeof estaChovendo);
+console.log(typeof "Teste");
