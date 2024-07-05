0bc25889fda3d88558a2d17770eebae1e349af67
nanic <131386211+nanic1@users.noreply.github.com>
2024-07-05 16:57:27 -0300

    Delete modulo 1 directory

--- modulo 1/dados3.js
+++
@@ -1,9 +0,0 @@
-let idade = 31;
-let salario = 4578.32;
-let estaChovendo = true // ou false
-
-console.log(typeof 31);
-console.log(typeof idade);
-console.log(typeof salario);
-console.log(typeof estaChovendo);
-console.log(typeof "Teste");
--- modulo 1/desafio_sentenca_bloco.js
+++
@@ -1,9 +0,0 @@
-var a = 7;
-var b = 94;
-
-console.log("A1: " + a + " B1: " + b)
-
-var a = 94;
-var b = 7;
-
-console.log ("A2: " + a + " B2: " + b)
