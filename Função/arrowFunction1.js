238e9647542bcadf21f51832f5819d0c043b0388
nanic <131386211+nanic1@users.noreply.github.com>
2024-07-08 16:17:56 -0300

    Add files via upload

---
+++ Função/arrowFunction1.js
@@ -0,0 +1,17 @@
+let dobro = function(a) {
+    return 2 * a
+}
+
+dobro = (a) =>{
+    return 2 * a
+}
+
+dobro = a => 2 * a
+console.log(dobro(Math.PI))
+
+let ola = function() {
+    console.log("Olá")
+}
+
+ola = () => "Olá"
+console.log(ola())
